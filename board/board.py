945f2346e0dedf781f15c0e58cbf811aff18e302
Trent Thorne <tthor021@odu.edu>
2026-04-18 19:27:34 -0400

    testing board

--- board/board.py
+++ board/board.py
@@ -1,5 +1,40 @@
+import pygame
 def Board():
     print("I am the board")
+    # Initialize Pygame
+    pygame.init()
+   
+    WIDTH = 1000
+    HEIGHT = 900
+
+    screen = pygame.display.set_mode([WIDTH, HEIGHT])
+
+    # Set up the game window
+    # screen = pygame.display.set_mode((400, 300))
+    pygame.display.set_caption("Chess")
+
+    # Game loop
+    running = True
+    while running:
+        for i in range(32):
+            column = i % 4
+            row = i // 4
+            if row % 2 == 0:
+                pygame.draw.rect(screen, 'light gray', [
+                                600 - (column * 200), row * 100, 100, 100])
+            else:
+                pygame.draw.rect(screen, 'light gray', [
+                                700 - (column * 200), row * 100, 100, 100])
+            pygame.draw.rect(screen, 'gray', [0, 800, WIDTH, 100])
+            pygame.draw.rect(screen, 'gold', [0, 800, WIDTH, 100], 5)
+            pygame.draw.rect(screen, 'gold', [800, 0, 200, HEIGHT], 5)
+        # for event in pygame.event.get():
+        #     if event.type == pygame.QUIT:
+        #         running = False
+
+    # Quit Pygame
+    pygame.quit()
+    
     
 # a-h
 # 1-8
--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+from board import board
 def main():
     print("let's play chess")
+    board.Board()
+
+if __name__ == "__main__":
+    main()
